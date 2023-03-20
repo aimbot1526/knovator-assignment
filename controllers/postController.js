0c8ba182c3a3a20fc1f81f21209ce54aff2c334e
aimbot1526 <anshjaamwal2002@gmail.com>
2023-03-21 02:00:44 +0530

    build: count & find apis

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 exports.Create = async (req, res) => {
     const { title, body, status, coordinates } = req.body;
     const user = await User.findOne({ _id: req.params.id })
-    if(!user) res.status(400);
+    if (!user) res.status(400);
     const newPost = await Post.create({
         title: title,
         body: body,
@@ -15,7 +15,7 @@ exports.Create = async (req, res) => {
             coordinates: coordinates
         }
     })
-    if(newPost) {
+    if (newPost) {
         res.status(200).send(newPost);
     } else {
         res.status(400);
@@ -24,7 +24,7 @@ exports.Create = async (req, res) => {
 
 exports.View = async (req, res) => {
     const post = Post.findOne({ _id: req.params.id })
-    if(!post) res.status(400);
+    if (!post) res.status(400);
     res.status(200).send(post);
 }
 
@@ -32,30 +32,33 @@ exports.Update = async (req, res) => {
     const post = req.body;
     const id = req.params.id;
     try {
-        if(post.title !== undefined && post.title !== '') {
+        if (post.title !== undefined && post.title !== '') {
             await Post.updateOne(
                 { _id: id },
                 { title: post.title }
             )
         }
-        if(post.body !== undefined && post.body !== '') {
+        if (post.body !== undefined && post.body !== '') {
             await Post.updateOne(
                 { _id: id },
                 { body: post.body }
             )
         }
-        if(post.status !== undefined && post.status !== '') {
+        if (post.status !== undefined && post.status !== '') {
             await Post.updateOne(
                 { _id: id },
                 { status: post.status }
             )
         }
-        if(post.location.coordinates !== undefined && post.location.coordinates !== '') {
-            const coordinates = { location: { coordinates: post.location.coordinates } };
-            console.log(coordinates)
+        if (post.location.coordinates !== undefined && post.location.coordinates !== '') {
             await Post.updateOne(
                 { _id: id },
-                { location: { coordinates: coordinates } }
+                {
+                    location: {
+                        type: "Point",
+                        coordinates: post.location.coordinates
+                    }
+                }
             )
         }
     } catch (error) {
@@ -68,5 +71,33 @@ exports.Update = async (req, res) => {
         location: {
             coordinates: post.location.coordinates
         }
+    })
+}
+
+exports.Delete = async (req, res) => {
+    const post = await Post.findOne({ _id: req.params.id })
+    if(!post) res.status(400).send();
+    await Post.deleteOne({ _id: post._id })
+    res.status(200).send("Deleted Successfully.")
+};
+
+exports.findByCoordinates = async (req, res) => {
+    const post = await Post.findOne({
+        location: {
+            type: "Point",
+            coordinates: req.body.coordinates
+        }
+    })
+    if(!post) res.status(400).send()
+    res.status(200).send(post)
+}
+
+exports.postCount = async (req, res) => {
+    const inactivePosts = await Post.countDocuments({ status: "Inactive" })
+    const activePosts = await Post.countDocuments({ status: "Active" })
+    if(!inactivePosts && !activePosts) res.status(400).send()
+    res.status(200).json({
+        activePosts: activePosts,
+        inactivePosts: inactivePosts
     })
 }
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -6,6 +6,9 @@ const postController = require('../controllers/postController')
 router.post('/create/:id', auth, postController.Create)
 router.get('/view/:id', auth, postController.View)
 router.post('/update/:id', auth, postController.Update)
+router.delete('/delete/:id', auth, postController.Delete)
+router.get('/geoloc', auth, postController.findByCoordinates)
+router.get('/postcount', auth, postController.postCount)
 
 router.get('/check', auth, (req, res) => {
     res.send("working")
